8092a852ef9095063ebf32e53709a04af5ed66ee
Tanmay Mazumdar <tanmay.mazumdar@quokkalabs.com>
2024-03-20 21:53:58 +0530

    (feat) Add brotli compression in build script.

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,21 @@ import { vitePlugin as remix } from '@remix-run/dev'
 import { installGlobals } from '@remix-run/node'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import viteCompression from 'vite-plugin-compression'
 
 installGlobals()
 
 export default defineConfig({
-  plugins: [remix({ ssr: true }), tsconfigPaths()],
+  plugins: [remix({ ssr: true }), tsconfigPaths(), viteCompression({ algorithm: 'brotliCompress' })],
   build: {
     terserOptions: {
       format: {
-        comments: false
+        comments: false,
       },
       compress: true,
       mangle: true,
       toplevel: true,
-    }
+    },
   },
-  esbuild: { legalComments: 'none', }
+  esbuild: { legalComments: 'none' },
 })
