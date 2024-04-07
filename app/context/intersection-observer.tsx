06fd6cb7304bd4991a4fc525ce2bdd93b6895915
Tanmay Mazumdar <mazumdar.t19@gmail.com>
2024-04-07 18:53:27 +0530

    (feat) Implement intersection observer functionality.

---
+++ app/context/intersection-observer.tsx
@@ -0,0 +1,32 @@
+import type { PropsWithChildren } from 'react'
+import { createContext, useEffect, useRef, useState } from 'react'
+
+import useIntersection from '../hooks/use-intersection'
+
+export const IntersectionContext = createContext({ inView: true })
+
+export const IntersectionObserver = ({
+  children,
+  reset = false, // if value set to true - observed element will reappear every time it shows up on the screen
+}: Readonly<PropsWithChildren<{ reset: boolean }>>) => {
+  const [inView, setInView] = useState(false)
+  const intersectionRef = useRef(null)
+  const intersection = useIntersection(intersectionRef, {
+    threshold: 0,
+  })
+
+  useEffect(() => {
+    const inViewNow = intersection && intersection.intersectionRatio > 0
+    if (inViewNow) {
+      return setInView(inViewNow)
+    } else if (reset) {
+      return setInView(false)
+    }
+  }, [intersection, reset])
+
+  return (
+    <IntersectionContext.Provider value={{ inView }}>
+      <div ref={intersectionRef}>{children}</div>
+    </IntersectionContext.Provider>
+  )
+}
